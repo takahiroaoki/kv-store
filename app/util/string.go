bb725387fd8fe9bccd5bd662c1c8b0b9d9f2909b
takahiroaoki <69064981+takahiroaoki@users.noreply.github.com>
2025-09-09 09:51:50 +0000

    ⬆️ go version up

--- app/util/string.go
+++ app/util/string.go
@@ -2,7 +2,7 @@ package util
 
 func PadStart(str string, length int, padStr string) string {
 	padding := ""
-	for len(padding)+len(str) < length-len(padStr) {
+	for len(padding)+len(str) <= length-len(padStr) {
 		padding += padStr
 	}
 	padding += padStr[:length-len(padding)-len(str)]
